438c61ba607c521a5bb3df417b3dbf18fb25d278
Allen Li <allen.li@berkeley.edu>
2013-03-10 23:58:14 -0700

    Improvements made to Board

--- player/Board.java
+++ player/Board.java
@@ -1,47 +1,66 @@
-/* Board.java */
+/* Board.java  */
+package player;
+
+import player.Move;
 
 /**
- * A public class for interfacing with a network board.
+ * A public class for storing a Board.
  */
 
 public class Board {
 
-    private int[][] b;
-    public static int BLACK = 0;
-    public static int WHITE = 1;
-    public static int EMPTY = -1;
+	private int[][] b;
+	private int nextPlayer;
+	public static int BLACK = 0;
+	public static int WHITE = 1;
+	public static int EMPTY = -1;
 
-    public Board() {
-        this.b = new int[8][8];
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                b[i][j] = EMPTY; //initialize all spaces to EMPTY
-            }
-        }
-    }
+	public Board() {
+		this(8);
+	}
 
-    public Board(int length) {
-        this.b = new int[length][length];
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-                b[i][j] = EMPTY; //initialize all spaces to EMPTY
-            }
-        }
-    }
+	public Board(int length) {
+		nextPlayer = WHITE;
+		this.b = new int[length][length];
+		for (int i = 0; i < length; i++) {
+			for (int j = 0; j < length; j++) {
+				b[i][j] = EMPTY; // initialize all spaces to EMPTY
+			}
+		}
+	}
 
-    //Decides if the Move m is a valid move on this board
-    public boolean validMove(Move m) {}
+	/**
+	 * Decides if the Move m is a valid move on this board
+	 * 
+	 * @param m
+	 * @return Whether the move is valid
+	 */
+	public boolean validMove(Move m) {
+		return false;
+	}
 
-    //Returns true if a player has won, false if not.
-    public boolean winner() {}
+	/**
+	 * Returns true if a player has won, false if not.
+	 * 
+	 * @return Whether the player has won
+	 */
+	public boolean winner() {
+		return false;
+	}
 
-    //Executes the Move m in the parameter if it is valid.
-    public void doMove(Move m) {}
+	/**
+	 * Executes the Move m in the parameter if it is valid.
+	 * 
+	 * @param m
+	 */
+	public void doMove(Move m) {
+	}
 
-    public String toString {}
+	public int getNextPlayer() {
+		return nextPlayer;
+	}
 
-    public static void main(String[] args) {}
+	public String toString() {
+		return super.toString();
+	}
 }
-    
-    
-
